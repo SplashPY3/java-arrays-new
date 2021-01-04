5a6c96db9fd2ebc5afb959265ec9f9fd26a2a991
Max <max.hacicheant@gmaigit config --global user.email max.hacicheant@gmai>
2021-01-04 11:53:52 +0200

    Added the Best Day to shop algorithm

--- src/Array4.java
+++ src/Array4.java
@@ -1,8 +1,6 @@
 public class Array4 {
     public static void main(String[] args) {
-        String[] shops = new String[7];
         int[] prices = {1050,2000,900,1500,895,2700,1000};
-        int[] discounts = {10, 20, 5, 15, 2, 25, 2};
         float res1 = prices[0] * 0.1f;
         float res2 = prices[1] * 0.2f;
         float res3 = prices[2] * 0.05f;
@@ -12,7 +10,22 @@ public class Array4 {
         float res7 = prices[6] * 0.02f;
         float[] results = {res1, res2, res3, res4, res5, res6, res7};
         for (int i = 0; i < prices.length; i++) {
-            System.out.println(prices[i] - results[i]); // this is straight up
+            System.out.println(prices[i] - results[i]);
+        }
+        float Monday = 945f;
+        float Tuesday = 1600f;
+        float Wednesday = 855f;
+        float Thursday = 1275f;
+        float Friday = 877.1f;
+        float Saturday = 2025f;
+        float Sunday = 980f;
+        float[] days = {Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday};
+        for (int j = 0; j < days.length; j++) {
+            if (days[j] < days[4]) {
+                String DiscountPrice = Float.toString(days[j]);
+                String Day = "Wednesday";
+                System.out.println("the best day to shop is " + Day + " and the price is " + DiscountPrice);
+            }
         }
     }
 }
